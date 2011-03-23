ec1a907d6c9ffae1cfddc2d3fa68a681f7bce35f
eric britsman <eric_britsman@hotmail.com>
2011-03-23 12:15:22 +0100

    minor changes

--- angle.c
+++ angle.c
@@ -6,13 +6,13 @@
  Description : radians to degree conversion based on two points
  ============================================================================
  */
-#include ("path_data_structure.h");
+#include "path_data_structure.h";
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 double calc(position a, position b) {
-	double angle = ((b.lon - a.lon)/(b.lat - a.lat)) * (180/ 3.14) + 180;
+	double angle = ((b.lon - a.lon)/(b.lat - a.lat)) * (180/ M_PI) + 180;
 	printf("%f\n", angle);
 	return angle;
 }
