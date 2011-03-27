efd11de62c0489f523587da9dd55579385365c3c
Eric Britsman <gusbriter@student.gu.se>
2011-03-27 13:10:15 +0200

    minor fix

--- angle.c
+++ angle.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 double calc(position a, position b) {
-	double angle = atan2((b.lon - a.lon),(b.lat - a.lat)) * (180/ M_PI) + 180;
+	double angle = atan2((b.lat - a.lat),(b.lon - a.lon)) * (180/ M_PI) + 180;
 	printf("%f\n", angle);
 	return angle;
 }
