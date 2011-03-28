21688eec50a0312712a9c1764de440036e9d7333
Joakim Gross <joakimgross@hotmail.com>
2011-03-28 11:57:16 +0200

    stubs files

---
+++ sched/src/stubs.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int caRun(void){
+    printf("inside caRun\n");
+    return 1;
+}
+
+int stabRun(void){
+    printf("inside stabRun\n");
+    return 1;   
+}
+
+int moveRun(void){
+    printf("inside moveRun\n");
+    return 1;   
+}
+
+int motoRun(void){
+    printf("inside motoRun\n");
+    return 1;
+}
+
+int protoRun(void){
+    printf("inside protoRun\n");
+    return 1;
+}
