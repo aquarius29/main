b3368f2cd0e4691a9888f8493b9973617bab1faf
Justin Wagner <justin.m.wagner@gmail.com>
2011-03-28 11:55:42 +0200

    here is the crap file I made to try and visualize the pseudo code.

---
+++ b_sched.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/*
+This is just a conceptualization of the pseudo code Joakim and I discussed earlier.  Needs much refinement or broken apart.  the defines should be moved to a header file.
+
+The defines will be the method call that provides the entry into the named module's code.  
+ */
+
+#define MOTOR Motor m
+#define MOVEMENT Move v
+#define COLLISION Collision c
+#define STABILIZAION Stab s
+
+typedef Array fun_arr[20];
+
+int main(void)
+{
+  long fun_arr[20];
+  int i;
+
+  printf("This program currently does nothing. And I mean nothing.\n");
+  while(i != '\0')
+    {
+      fun_arr[i];
+    }
+}
