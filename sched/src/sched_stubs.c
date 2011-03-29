2865acb450698af480c0881bcee71d0023424da3
Joakim Gross <joakimgross@hotmail.com>
2011-03-29 10:54:08 +0200

    changes in ifdef style

--- sched/src/sched_stubs.c
+++ sched/src/sched_stubs.c
@@ -1,58 +1,47 @@
 
 #include <stdio.h>
 
-#ifdef ARDUINO
-    #define ARDUINO_F 1
-#else
-    #define ARDUINO_F 0
-#endif
-
 int caRun(void){
-    if(ARDUINO_F){
-
-    }
-    else{
-        printf("inside caRun\n");        
-    }
+    #ifdef ARDUINO
+    
+    #else
+        printf("inside caRun\n")
+    #endif
     return 1;
 }
 
 int stabRun(void){
-    if(ARDUINO_F){
-        
-    }
-    else{
-        printf("inside stabRun\n");
-    }
-    return 1;   
+    #ifdef ARDUINO
+    
+    #else
+        printf("inside stabRun\n")
+    #endif
+    return 1;
 }
 
 int moveRun(void){
-    if(ARDUINO_F){
-
-    }
-    else{
-        printf("inside moveRun\n");        
-    }
-    return 1;   
+    #ifdef ARDUINO
+    
+    #else
+        printf("inside moveRun\n")
+    #endif
+    return 1;
 }
 
 int motoRun(void){
-    if(ARDUINO_F){
-
-    }
-    else{
-        printf("inside motoRun\n");        
-    }
+    #ifdef ARDUINO
+    
+    #else
+        printf("inside motoRun\n")
+    #endif
     return 1;
 }
 
 int protoRun(void){
-    if(ARDUINO_F){
-
-    }
-    else{
-        printf("inside protoRun\n");    
-    }
+    #ifdef ARDUINO
+    
+    #else
+        printf("inside protoRun\n")
+    #endif
     return 1;
 }
