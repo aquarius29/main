4af3d7efd7681be4b5a33aadcb4c4f3bec209cdc
Anders <anders@art-studio.se>
2011-03-29 14:56:01 +0200

    Scheduler prototype

---
+++ sched/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "scheduler.h"
+#include "sched_stubs.h"
+
+/* NOTE: Most of this module is hardcoded, prototype implementation */
+
+Process *g_processArr[NO_PROCESSES];
+
+void setup(void)
+{
+	Process *process = createProcess("MotorProcess");
+	Task *task = createTask("runMotor", &motoRun);
+	enqueueTask(process, task);
+	g_processArr[0] = process;
+}
+
+void loop(void)
+{
+	int i;
+	for(i = 0; i < 1; i++)
+	{
+		Process *process = g_processArr[i];
+		process->idleTask->functionPointer();
+	}
+}
+
+int main(void)
+{
+	setup();
+	loop();
+}
