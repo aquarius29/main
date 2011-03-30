2d934566d93158c99d032bc88b119a131e3242ed
Justin Wagner <justin.m.wagner@gmail.com>
2011-03-30 13:00:23 +0200

    real add of fly.c mods

--- sched/fly.c
+++ sched/fly.c
@@ -8,66 +8,71 @@ in case of wrong
 
 #include<stdio.h>
 #include"fly.h"
+
 int main(int argc, char **argv){
+
 int a =1;
-int collid,mot,stab,nav,connect,cam;
-if (argc < 2){
-printf("you should enter argument to test \n");
-return;
+ int collid,mot,stab,nav,connect,cam;
+
+ if (argc < 2){
+   printf("you should enter argument to test \n");
+   return;
 }
-collid= atoi(argv[1]);
-mot = atoi(argv[2]);
-stab= atoi(argv[3]);
-nav= atoi(argv[4]);
-connect= atoi(argv[5]);
-cam= atoi(argv[6]);
+
+
+ collid= atoi(argv[1]);
+ mot = atoi(argv[2]);
+ stab= atoi(argv[3]);
+ nav= atoi(argv[4]);
+ connect= atoi(argv[5]);
+ cam= atoi(argv[6]);
  while(a==1 ){
-collidence(collid);
-motor(mot);
-stablization(stab);
-navigation(nav);
-connectivity(connect);
-camera(cam);
-//set a to zero to stop the loop 
-a = 0; 
-}
-return 0;
+   collidence(collid);
+   motor(mot);
+   stablization(stab);
+   navigation(nav);
+   connectivity(connect);
+   camera(cam);
+   //set a to zero to stop the loop 
+   a = 0; 
+ }
+ return 0;
 }
 
 
 void collidence(int x){
-// if this function gets (0) as argument it will flag ( show ) error for collidence
-if(x == 0){
-printf("movement NOT GOOD     ---------->->->\n");
-}
-// the normal flying no collidence possibilty arguments can be any thing than (0)
-else {
-printf("movement is  OK\n");
-    }
+  // if this function gets (0) as argument it will flag ( show ) error for collidence
+  if(x == 0){
+    printf("movement NOT GOOD     ---------->->->\n");
+  }
+  // the normal flying no collidence possibilty arguments can be any thing than (0)
+  else {
+    printf("movement is  OK\n");
+  }
 }
 
 
 void motor(int x){
-// if this function gets (0) as argument it will flag ( show ) error with motors
-if(x == 0){
-printf("motor  NOT GOOD      ---------->->->\n");
-      }
-// the normal flying no collidence possibilty arguments should be more than one 
-else {
-printf("motor is  OK\n");
-    }
+  // if this function gets (0) as argument it will flag ( show ) error with motors
+  if(x == 0){
+    printf("motor  NOT GOOD      ---------->->->\n");
+  }
+  // the normal flying no collidence possibilty arguments should be more than one 
+  else {
+    printf("motor is  OK\n");
+  }
 }
 
 
 void stablization(int x){
-// if this function gets (0) as argument it will flag ( show ) error with stablization
-if(x == 0){
-printf("stablization NOT GOOD ---------->->->\n");
-      }
-// the normal stablization , arguments can be any thing than (0) 
-else {
-printf("stablization is  OK\n");
-    }
+  // if this function gets (0) as argument it will flag ( show ) error with stablization
+  if(x == 0){
+    printf("stablization NOT GOOD ---------->->->\n");
+  }
+  // the normal stablization , arguments can be any thing than (0) 
+  else {
+    printf("stablization is  OK\n");
+  }
 }
 
 
@@ -84,25 +89,25 @@ printf("navigation is    OK \n");
 
 
 void connectivity(int x){
-// if this function gets (0) as argument it will flag ( show ) error for connectivity
-if(x == 0){
-printf("connectivity NOT GOOD ---------->->->\n");
-      }
-// the normal connectivity , arguments can be any thing than (0)
-else {
-printf("connectivity is   OK\n");
-    }
+  // if this function gets (0) as argument it will flag ( show ) error for connectivity
+  if(x == 0){
+    printf("connectivity NOT GOOD ---------->->->\n");
+  }
+  // the normal connectivity , arguments can be any thing than (0)
+  else {
+    printf("connectivity is   OK\n");
+  }
 }
 
 
 void camera(int x){
-// if this function gets (0) as argument it will flag ( show ) errore for camera action
-if(x == 0){
-printf("camera NOT GOOD     ---------->->->\n");
-      }
-// the normal camera action , arguments can be any thing than (0)
-else {
-printf("camera is       OK\n");
-    }
+  // if this function gets (0) as argument it will flag ( show ) errore for camera action
+  if(x == 0){
+    printf("camera NOT GOOD     ---------->->->\n");
+  }
+  // the normal camera action , arguments can be any thing than (0)
+  else {
+    printf("camera is       OK\n");
+  }
 }
 
