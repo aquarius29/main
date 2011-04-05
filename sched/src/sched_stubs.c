f5444b4b4e4d70bbe945d889805e182b133045bb
Joakim Gross <joakimgross@hotmail.com>
2011-04-05 08:33:13 +0200

    put src/ folder back

---
+++ sched/src/sched_stubs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include "sched_stubs.h"
+
+int caRun(void)
+{
+	printf("inside caRun\n");
+	return 0;
+}
+
+int stabRun(void)
+{
+	printf("inside stabRun\n");
+	return 0;
+}
+
+int moveRun(void)
+{
+	printf("inside moveRun\n");
+	return 0;
+}
+
+int motoRun(void)
+{
+	printf("inside motoRun\n");
+	return 0;
+}
+
+int protoRun(void)
+{
+	printf("inside protoRun\n");
+	return 0;
+}
