c6c24e390b7c69f1167c6b1babc341e71133c42c
NELSON S <nellysek@ubuntu.(none)>
2011-04-07 15:39:15 -0700

    update 4 startoff,liftoff,land cd

---
+++ conn/src/conn_command.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+ * @author: mgbah iheanacho
+ * @file: command.c
+ *
+ */
+
+/*
+ * command to start up the drone
+ */
+startUp(){
+  printf("starting up\n");
+}
+
+/*
+ * command to lift off
+ */
+liftOff(){
+  printf("lift off\n");
+}
+
+/*
+ * command to land
+ */
+landSafely(){
+  printf("landing\n");
+}
