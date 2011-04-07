d0304da102f6c2b9889813ba108875c75ba703ac
Adam Debbiche <adam.debbiche@gmail.com>
2011-04-07 15:28:09 +0200

    Comitting code of stab

---
+++ stab/src/stab_rg.c
@@ -0,0 +1,13 @@
+#include "stab.h"
+
+
+float read_gyro()
+{
+  
+  
+
+
+  
+  return 0;
+
+}
---
+++ stab/src/stab_sched.h
@@ -0,0 +1,13 @@
+/*
+  Header file used as an interface for the scheduler
+
+
+ */
+
+
+
+
+void stab_init(void);
+
+
+void stab_start(void);
