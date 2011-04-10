e508be8c46cedca19c4d1bf5b4e5b1c716d356c1
e0 <zero42e@gmail.com>
2011-04-10 21:34:57 +0200

    Start on testing.

---
+++ src/path_structure.h
@@ -0,0 +1,45 @@
+/*
+ * path_structure.h
+ *
+ * Authors: Jon Fridéhn Eric Britsman Shan Huang
+ */
+#ifndef JOURNEYTRACKING_H
+#define JOURNEYTRACKING_H
+
+#define COORDINATE_LIMIT 4
+#include <sys/time.h>
+
+typedef struct{
+    double lat;
+    double lon;
+    double angle;
+    double distance;
+} position;
+
+typedef struct{
+	position ending_point;
+	position current_destination;
+	position current_point;
+    position *path;
+    struct timeval timer;
+    unsigned int num;
+} progressive_route;
+
+typedef struct{
+	position ending_point;
+	position starting_point;
+    position *path;
+    unsigned int num;
+} dijkstra_route;
+
+void DynAddCoordinates(float lat, float lon);
+void StaAddCoordinates(float lat, float lon);
+void FreePointer(dijkstra_route * route);
+void Test();
+void set_direction(progressive_route *path);
+void set_distance(progressive_route *path);
+void update_position(progressive_route *path);
+void init_path(position start, position end);
+void navigate_path();
+
+#endif /* JOURNEYTRACKING_H */
---
+++ test/unit/unitmain.c
@@ -0,0 +1,40 @@
+/*
+* unitmain.c
+*
+*  Created on: 7 apr 2011
+*      Author: Shan Huang
+*/
+
+#include "CUnit/Basic.h"
+
+#include "simulations.c"
+
+int main() {
+    CU_pSuite simSuite = NULL;
+
+  /* Initialize the CUnit test registry */
+    if(CUE_SUCCESS != CU_initialize_registry())
+        return CU_get_error();
+
+  /* Add a suite to the registry */
+    simSuite = CU_add_suite("Simulations", init_suite, clean_suite);
+
+    if(	NULL == simSuite ) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+  /* Add tests to suite */
+    if(	NULL == CU_add_test(simSuite, "Single Destination", single_destination)
+        || NULL == CU_add_test(pSuite1, "Multiple Destinations", multiple_destinations)
+    ) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+  /* Run all tests using the CUnit Console interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
