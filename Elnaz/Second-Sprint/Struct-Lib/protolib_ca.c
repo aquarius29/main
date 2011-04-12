e8c1f6d66b96d01d5973de2c1648b5ca49603cd8
Erl-elnaz <elnaz.shahmehr@live.com>
2011-04-12 16:08:50 +0200

    struct lib for collision avoidance and movement

---
+++ Elnaz/Second-Sprint/Struct-Lib/protolib_ca.c
@@ -0,0 +1,66 @@
+/*****************************************************************************
+* Product: 
+* Version: 
+* Released: April 7 2011
+* Updated: April 12 2011
+*
+* Copyright (C) 2011 Elnaz Shahmehr
+*
+* 
+*
+* <IT University of Goteborg>
+*****************************************************************************/
+
+ 
+ #ifdef PC
+   #include <stdio.h>
+   #include "protolib_move.h" 
+#endif
+/***************************************************************************** 
+    Definitions 
+*****************************************************************************/ 
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+struct collision_avoid {
+	int height; // from Collision Avoidance Movement to; DEFAULT is 0
+	int distance ;
+	int direction; // from Collision Avoidance Movement to; DEFAULT is 0
+      
+} collision_avoid;
+
+
+
+ 
+ 
+//write speed from movement to collision avodance  
+void write(struct collision_avoid ca);// proto API 
+ 
+ 
+//read speed from movement to collision avodance 
+struct collision_avoid read();// proto API 
+ 
+/***************************************************************************** 
+    Implementations 
+*****************************************************************************/ 
+ 
+void write(struct collision_avoid ca){ 
+    collision_avoid = ca;
+#ifdef PC
+	printf("Movement value set to: %d , %d , %d\n", ca.height , ca.direction, ca.distance);
+#endif
+} 
+ 
+ 
+struct collision_avoid read(){ 
+#ifdef PC
+	printf("Movement value set to: %d , %d ,%d\n", ca.height , ca.direction ,ca.distance);
+#endif
+
+    return collision_avoid; 
+}
+
+
---
+++ Elnaz/Second-Sprint/Struct-Lib/protolib_move.c
@@ -0,0 +1,65 @@
+/*****************************************************************************
+* Product: 
+* Version: 
+* Released: April 8 2011
+* Updated: April 12 2011
+*
+* Copyright (C) 2011 Elnaz Shahmehr
+*
+* 
+*
+* <IT University of Goteborg>
+*****************************************************************************/
+
+ 
+ #ifdef PC
+   #include <stdio.h>
+   #include "protolib_move.h" 
+#endif
+/***************************************************************************** 
+    Definitions 
+*****************************************************************************/ 
+
+#include <stdio.h>
+#include <string.h>
+
+
+struct move {
+        int speed;
+        int direction;
+      
+} move_ca;
+
+ 
+//write speed from movement to collision avodance  
+void write(struct move mv);// proto API 
+ 
+ 
+//read speed from movement to collision avodance 
+struct move read();// proto API 
+ 
+
+ 
+/***************************************************************************** 
+    Implementations 
+*****************************************************************************/ 
+ 
+void write(struct move mv){ 
+    move_ca = mv;
+#ifdef PC
+	printf("Movement value set to: %d , %d\n", mv.speed , mv.direction);
+#endif
+} 
+ 
+ 
+struct move read(){ 
+#ifdef PC
+	printf("Movement value set to: %d , %d\n", mv.speed , mv.direction);
+#endif
+
+    return move_ca; 
+} 
+
+
+
+
