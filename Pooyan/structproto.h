f5d43bd61898c209a7a62c53b539d80b30ba1520
Erl-elnaz <elnaz.shahmehr@live.com>
2011-04-13 12:49:26 +0200

    lib

---
+++ Pooyan/structproto.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+* Product: 
+* Version:  
+* Released: April 5 2011
+* Updated: April 5 2011
+* 
+* Copyright (C) 2011 Navid amiriarshad,Pooyan Mobtahej
+*
+* Providing message handling Between Navigation and Connectivity
+*
+* <IT University of Goteborg>
+*****************************************************************************/
+
+
+
+
+       
+struct proto read();//API
+
+
+void write(struct proto msg);//API
