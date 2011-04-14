ada1d28ac29eb24cc3494cba0bca485785859c7b
Amber Olsson <amberolsson@gmail.com>
2011-04-14 01:18:14 -0700

    deleted extra file

--- magn/comp.h
+++
@@ -1,9 +0,0 @@
-void in(void);
-float calc_h(int pitch, int roll, int x, int y, int z);
-void getValues(int *x, int *y, int *z);
-void sendStart(void);
-void sendStop(void);
-void sendByte(unsigned char data);
-unsigned char receiveByte(void);
-unsigned char readStatus(void);
-void waitForReady(void);
