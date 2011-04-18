71e15bd0d862e9f169d21222af2673ae9556c2f3
Erl-elnaz <elnaz.shahmehr@live.com>
2011-04-18 20:38:12 +0200

    code Standard name

---
+++ src/ proto_ conn.c
@@ -0,0 +1,49 @@
+/***************************************************************************** 
+* Product:  
+* Version:   
+* Released: April 5 2011 
+* Updated: April 5 2011 
+*  
+* Copyright (C) 2011 Navid amiriarshad,Pooyan Mobtahej 
+* 
+* Providing message handling Between Navigation and Connectivity 
+* 
+* <IT University of Goteborg> 
+*****************************************************************************/ 
+ 
+#include<stdio.h> 
+#include<stdlib.h>  
+#include"proto_ conn.h" 
+ 
+ 
+/***************************************************************************** 
+    Definitions 
+*****************************************************************************/ 
+ 
+unsigned char navi_to_connectivity_msg; 
+//unsigned char nav_msg; 
+ 
+ 
+unsigned char read_navi_to_connectivity(void); 
+ 
+void send_navi_to_connectivity(unsigned char msg); 
+ 
+/***************************************************************************** 
+    Implementations 
+*****************************************************************************/ 
+ 
+ 
+unsigned char read_navi_to_connectivity(void){ 
+#ifdef PC
+	printf("navigation msg value set to: %c\n", navi_to_connectivity_msg );
+#endif
+    return navi_to_connectivity_msg; 
+    
+} 
+ 
+void send_navi_to_connectivity(unsigned char msg){ 
+    navi_to_connectivity_msg = msg; 
+#ifdef PC
+	printf("navigation msg value set to: %c\n", navi_to_connectivity_msg );
+#endif
+} 
