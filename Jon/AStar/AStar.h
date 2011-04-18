4de0743a9a17a4cd4af0fe9d0587e18c9ce258fc
Jon Fridehn <jon.fridehn@gmail.com>
2011-04-18 09:11:29 +0200

    AStar indoor implementation

---
+++ Jon/AStar/AStar.h
@@ -0,0 +1,50 @@
+/*
+ * Dijkstra.h
+ *
+ *  Created on: Apr 10, 2011
+ *      Author: Jon Fridehn
+ */
+
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#define MAP_Y 14
+#define MAP_X 12
+#define NUM_ADJACENT 8
+
+typedef struct{
+	int y;
+	int x;
+}position;
+
+typedef struct node
+{
+	position pos;
+	int totalCost;
+	int heuristic;
+	position previous;
+}node;
+
+typedef struct {
+	node * list;
+	int count;
+}nodeList;
+
+typedef struct {
+	position * list;
+	int count;
+}positionList;
+
+void AddNodeToOpen(node * current, nodeList * open);
+void RemoveNodeFromOpen(node * current, nodeList * open);
+void AddNeighborsToOpen(node * current, nodeList * open, nodeList * closed, int8_t map[][MAP_X], const position * end);
+void FreeAllocatedList(nodeList * list);
+positionList * AStar(const position * start, const position * end, int8_t map[][MAP_X]);
+int8_t ListMemoryAllocation(nodeList ** open, nodeList ** closed);
+positionList * CreateFinalList(nodeList * closed, const position * goal, const position * drone);
+int8_t NodeInClosed(const int * nodeY, const int * nodeX, nodeList * closed);
+int8_t NodeInOpen(const int * nodeY, const int * nodeX, nodeList * open);
+node * AddNodeToClosed(node * current, nodeList * closed);
+int32_t GetHeuristicCost(const position * currentNode, const position * goalNode);
+
+#endif	// DIJKSTRA_H
