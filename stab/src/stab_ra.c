d7b8904c56bdd905b5208e7b9f32d9a12a739432
Anton Shumeika <shumeikaanton@gmail.com>
2011-04-18 16:32:59 +0200

    adding comments to stab_ra.c

--- stab/src/stab_ra.c
+++ stab/src/stab_ra.c
@@ -1,40 +1,50 @@
-//#include "stab.h"
+/*
+ * 
+ *
+ *
+ */
+
 #include <Wire.h>
 #include "WProgram.h"
+#define ADDRESS 0x40   /* address of the accelerometer on the board */
 
-#define address 0x40   // address of the accelerometer on the board
+
 int x,y,z;
 int temp;
 
 
-//void initBMA180();
+void init_accel();
 void readAccel();
 void checkResult(int result);
 
-int main()
-//void setup()
-{
-  init();
-  Wire.begin();
-  Serial.begin(9600);
-// initBMA180() should be done once, it will write appropriate adjustments for range and mode. 
-//  initBMA180();
-//  delay(2000);
-	while(1){
-  readAccel();
-  delay(300);
-	}
-  return 0;
-}
 
+
+/*int main()*/
+/*//void setup()*/
+/*{*/
+/*  init();*/
+/*  Wire.begin();*/
+/*  Serial.begin(9600);*/
+/*// initBMA180() should be done once, it will write appropriate adjustments for range and mode. */
+/*//  initBMA180();*/
+/*//  delay(2000);*/
+/*	while(1){*/
+/*  readAccel();*/
+/*  delay(300);*/
+/*	}*/
+/*  return 0;*/
+/*}*/
+
+
+/*Read data from accelerometer memory*/
 void readAccel()
 {
-  unsigned int result;
+    unsigned int result;
   
-  Wire.beginTransmission(address);
-  Wire.send(0x02);
+  Wire.beginTransmission(ADDRESS);
+  Wire.send(0x02);  							/*start of x LSB for reading data*/
   Wire.endTransmission();
-  Wire.requestFrom((int)address, 7);
+  Wire.requestFrom((int)ADDRESS, 7);
   if(Wire.available()==7)
   {
     int lsb = Wire.receive()>>2;
@@ -53,27 +63,25 @@ void readAccel()
     if (temp&0x80) temp|=0xff00;
   }
   result = Wire.endTransmission();
-//  Serial.print("the result of transaction should be 0 and it is: ");
-//  Serial.println(result);
-  Serial.print("raw data: ");
-  Serial.print("X=");
-  Serial.print(x);
-  Serial.print(" Y=");
-  Serial.print(y);
-  Serial.print(" Z=");
-  Serial.print(z);
-  Serial.println();
+/*  Serial.print("raw data: ");*/
+/*  Serial.print("X=");*/
+/*  Serial.print(x);*/
+/*  Serial.print(" Y=");*/
+/*  Serial.print(y);*/
+/*  Serial.print(" Z=");*/
+/*  Serial.print(z);*/
+/*  Serial.println();*/
 }
 //============================
 
-void initBMA180()
+void init_accel()
 {
   int temp, result, error;
   int tempid;
   
-  Wire.beginTransmission(address);
+  Wire.beginTransmission(ADDRESS);
   Wire.send(0x00);
-  Wire.requestFrom(address, 1);
+  Wire.requestFrom(ADDRESS, 1);
   while(Wire.available())
   {
     temp = Wire.receive();
@@ -88,7 +96,7 @@ void initBMA180()
   if(temp == 3)
   {
     // Connect to the ctrl_reg1 register and set the ee_w bit to enable writing.
-    Wire.beginTransmission(address);
+    Wire.beginTransmission(ADDRESS);
     Wire.send(0x0D);
     Wire.send(B0001);
     result = Wire.endTransmission();
@@ -99,7 +107,7 @@ void initBMA180()
     }
     delay(10);
     // Connect to the bw_tcs register and set the filtering level to 10hz.
-    Wire.beginTransmission(address);
+    Wire.beginTransmission(ADDRESS);
     Wire.send(0x20);
     Wire.send(B00001000);
     result = Wire.endTransmission();
@@ -110,7 +118,7 @@ void initBMA180()
     }
     delay(10);
     // Connect to the offset_lsb1 register and set the range to +- 2.
-    Wire.beginTransmission(address);
+    Wire.beginTransmission(ADDRESS);
     Wire.send(0x35);
     Wire.send(B0100);
     result = Wire.endTransmission();
