cb9582415decad2573db77899aef1eac71b882eb
JonKristensen <technology@jonkristensen.com>
2011-04-19 10:09:04 +0200

    Forgot to add the new compile file.

--- v4l/compile.sh
+++ v4l/compile.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export PKG_CONFIG_PATH=/opt/ffmpeg/lib/pkgconfig
 rm *.o video-test &> /dev/null
-gcc -c v4l-test.c -o v4l-test.o
-gcc -o v4l-test v4l-test.o
+gcc `pkg-config --cflags libavformat libavcodec` -c v4l-test.c -o v4l-test.o
+gcc `pkg-config --cflags libavformat libavcodec` -c encode.c -o encode.o
+gcc `pkg-config --libs libavformat libavcodec` -o v4l-test v4l-test.o encode.o
