6c543b1705cdb6b986b6d31c0e38d9b90ecb90ed
Joakim Gross <joakimgross@hotmail.com>
2011-04-19 15:50:28 +0200

    changed in ca_sensor.c to try and fix math lib crash

--- ca/src/ca_sensor.c
+++ ca/src/ca_sensor.c
@@ -26,7 +26,8 @@ int ir_distance(int irpin)
   // if running 3.3.volts then change 5 to 3.3
 
   //convert to the distance according to the formula from luckylarry.co.uk
-  int distance = 65*pow(value*0.0048828125, -1.10);
+//  int distance = 65*pow(value*0.0048828125, -1.10);
+  int distance = 65*pow(1*0.0048828125, -1.10);
   return distance; 
 }
 
