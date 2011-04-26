a2ed5cca2992ab4d08fc0dfb74ed5ca1640bac23
Amber Olsson <amberolsson@gmail.com>
2011-04-26 01:37:29 -0700

    added protocol comments in intercae

--- ca/src/ca_interface.c
+++ ca/src/ca_interface.c
@@ -69,16 +69,23 @@ int ca_init(void)
 int ca_run(void)
 {
 
+  //Protocol
+  // int direction = read_direction(void);
+  // int speed = read_speed(void);
   int ir1=100;
   int ir2=100;
   int ir3=100;
   int ir4=100;
   int i=0;
-  while (i<5){
+  /* while (i<5){
     direction_filter(get_speed(),get_dir(),ir1, ir2, ir3, ir4);
     i++;
-  }
+    }*/
 
+   int direction = direction_filter(speed,direction,ir1, ir2, ir3, ir4);
+
+  //Protocol 
+  //write(direction);
   return 1;
 }
 
