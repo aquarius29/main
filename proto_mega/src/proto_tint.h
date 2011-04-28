ab6878b73307095fec98db34f87b1a05c5edd001
Mihai A <bestmikall@gmail.com>
2011-04-28 12:17:11 +0200

    Changes for TINT

---
+++ proto_mega/src/proto_tint.h
@@ -0,0 +1,40 @@
+/*****************************************************************************
+* Product: 
+* Version: 
+* Released: April 28 2011
+* Updated: April 28 2011
+*
+* Copyright (C) 2011 Petre Mihail Anton
+*
+* <IT University of Goteborg>
+*****************************************************************************/
+#ifdef PC
+
+#define READ 0
+#define WRITE 1
+
+#define STAB 3
+#define COLLISION 4
+#define NAV 5
+#define MOTOR 6
+#define MOVEMENT 7
+#define CONNECTIVITY 8
+#define CAMERA 9
+
+#define UNKNOWN -1
+
+struct Message {
+	int operation;
+	int source;
+	int meaning;
+	double value;
+	struct Message *next;
+} * mPtr;
+
+void storeForTint(int operation, int source, int meaning, double value);
+
+struct Message * retrieve(void);
+
+void flush(void);
+
+#endif
