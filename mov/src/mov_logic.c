ab12f989054a954422c0cc353c09203dc295b0df
Yanling Jin <jjisjj@gmail.com>
2011-05-02 10:30:15 +0200

    add functions according to motor protocol

--- mov/src/mov_logic.c
+++ mov/src/mov_logic.c
@@ -13,9 +13,8 @@
 #include <stdio.h>
 #include "mov_interface.h"
 
-
-// BitMasking example http://www.indiabix.com/technical/c/bits-and-bytes/
-#define BIT_POS(N)              ( 1U << (N) )
+/* BitMasking example http://www.indiabix.com/technical/c/bits-and-bytes/ */
+#define BIT_POS(N)            ( 1U << (N) )
 #define SET_FLAG(N, F)        ( (N) |= (F) )
 #define CLR_FLAG(N, F)        ( (N) &= -(F) )
 
@@ -39,29 +38,224 @@ void testNavCommand(void) {
     printf("==============================\n");
 }
 
-//************************************************************
-// 
-// 0100 0000 (start)
-//************************************************************
-void start_motors(void){
-
-    char msg = to_MotorMessage(0,1,0,0,0,0,0,0);
-    pWrite(msg);
+/* 
+ * 01 00 00 00
+ */
+void start_motors(void)
+{
+	char msg = to_MotorMessage(0,1,0,0,0,0,0,0);
+	pWrite(msg);
 }
 
-//************************************************************
-//
-//  0000 0000 (stop)
-//************************************************************
-void stop_motors(void){
-
-    char msg = to_MotorMessage(0,0,0,0,0,0,0,0);
-    pWrite(msg);
+/*
+ *  00 00 00 00
+ */
+void stop_motors(void)
+{
+	char msg = to_MotorMessage(0,0,0,0,0,0,0,0);
+	pWrite(msg);
 }
+
+/*
+ *  10 10 00 01
+ */
+void increase_rear_motor(void){
+	char msg = to_MotorMessage(1,0,1,0,0,0,0,1);
+	pWrite(msg);
+
+}
+
+/*
+ *  10 00 00 01
+ */
+void decrease_rear_motor(void){
+
+	char msg = to_MotorMessage(1,0,0,0,0,0,0,1);
+	pWrite(msg);
+}
+
+/*
+ *  10 10 00 10
+ */
+void increase_front_motor(void){
+
+	char msg = to_MotorMessage(1,0,1,0,0,0,1,0);
+	pWrite(msg);
+}
+
+/*
+ *  10 00 00 10
+ */
+void decrease_front_motor(void){
+
+	char msg = to_MotorMessage(1,0,0,0,0,0,1,0);
+	pWrite(msg);
+}
+
+/*
+ *  10 10 01 00
+ */
+void increase_left_motor(void){
+	char msg = to_MotorMessage(1,0,1,0,0,1,0,0);
+	pWrite(msg);
+
+}
+
+
+/*
+ *  10 00 01 00
+ */
+void decrease_left_motor(void){
+
+	char msg = to_MotorMessage(1,0,0,0,0,1,0,0);
+	pWrite(msg);
+}
+
+
+/*
+ *  10 10 10 00
+ */
+void increase_right_motor(void){
+
+	char msg = to_MotorMessage(1,0,1,0,1,0,0,0);
+	pWrite(msg);
+}
+
+
+/*
+ *  10 00 10 00
+ */
+void decrease_right_motor(void){
+	char msg = to_MotorMessage(1,0,0,0,1,0,0,0);
+	pWrite(msg);
+
+}
+
+
+/*
+ *  11 10 00 11
+ */
+void increase_rear_decrease_front(void){
+
+	char msg = to_MotorMessage(1,1,1,0,0,0,1,1);
+	pWrite(msg);
+}
+
+
+/*
+ *  11 00 00 11
+ */
+void increase_front_decrease_rear(void){
+	char msg = to_MotorMessage(1,1,0,0,0,0,1,1);
+	pWrite(msg);
+
+}
+
+/*
+ *  11 10 11 00
+ */
+void increase_left_decrease_right(void){
+	char msg = to_MotorMessage(1,1,1,0,1,1,0,0);
+	pWrite(msg);
+}
+
+/*
+ *  11 00 11 00
+ */
+void incease_right_decrease_left(void){
+
+	char msg = to_MotorMessage(1,1,0,0,1,1,0,0);
+	pWrite(msg);
+}
+
+/*
+ *  10 10 11 11
+ */
+void increase_all(void){
+	char msg = to_MotorMessage(1,0,1,0,1,1,1,1);
+	pWrite(msg);
+
+}
+
+/*
+ *  10 00 11 11
+ */
+void decrease_all(void){
+	char msg = to_MotorMessage(1,0,0,0,1,1,1,1);
+	pWrite(msg);
+}
+
+
+/*
+ * 11 00 00 10
+ */
+void go_forwards(void)
+{
+	char msg = to_MotorMessage(1,1,0,0,0,0,1,0);
+	pWrite(msg);
+}
+
+/*
+ * 11 00 00 01 
+ */
+void go_backwards(void)
+{
+
+	char msg = to_MotorMessage(1,1,0,0,0,0,0,1);
+	pWrite(msg);
+}
+
+/*
+ * 11 10 01 00
+ */
+void strafe_left(void)
+{
+	char msg = to_MotorMessage(1,1,1,0,0,1,0,0);
+	pWrite(msg);
+}
+
+
+/*
+ * 11 00 10 00
+ */
+void strafe_right(void)
+{
+	char msg = to_MotorMessage(1,1,0,0,1,0,0,0);
+	pWrite(msg);
+}
+
+/*
+ *  11 10 11 00
+ */
+void turn_left(void){
+	char msg = to_MotorMessage(1,1,1,0,1,1,0,0);
+	pWrite(msg);
+
+}
+
+/*
+ *  11 00 11 00
+ */
+void turn_right(void){
+	char msg = to_MotorMessage(1,1,0,0,1,1,0,0);
+	pWrite(msg);
+
+
+}
+
+/*
+ *  11 11 11 11
+ */
+void hover(void)
+{
+	char msg =to_MotorMessage(1,1,1,1,1,1,1,1);
+	pWrite(msg);
+}
+
 
 //check the drone height
-void check_height(void){
-
+void check_height(void)
+{
 	int height_desire =navCommand.height;
 	int height_current =sensorCommand.height;
 
@@ -78,7 +272,9 @@ void check_height(void){
 	}
 }
 
-void check_heading(void){
+void check_heading(void)
+{
+
 
 	int heading_desire=navCommand.yaw;
 	int heading_current=sensorCommand.yaw;
@@ -131,131 +327,71 @@ void check_pitch_roll(int isHovering) {
 	}
 }
 
-//************************************************************
-//
-//  1111 1111 (hover)
-//************************************************************
-void hover(void){
-
-    char msg =to_MotorMessage(1,1,1,1,1,1,1,1);
-    pWrite(msg);
-}
-
-
-//************************************************************
-//
-//Go Left without turning (Strafe)
-//0110 1100 (left, decreases left motor and increases right motor)
-//************************************************************
-void go_left_no_strafe(void){
-
-    char msg = to_MotorMessage(0,1,1,0,1,1,0,0);
-    pWrite(msg);
-}
-
-
-//************************************************************
-//
-//Go Right without turning (Strafe)
-//0100 1100 (right, increases left motor and decreases right motor)
-//************************************************************
-void go_right_no_strafe(void){
-
-    char msg = to_MotorMessage(0,1,0,0,1,1,0,0);
-    pWrite(msg);
-}
-
-
-//************************************************************
-//Go Forwards
-//
-//0110 0011 (forward, decreases front motor and increases rear motor)
-//************************************************************
-void go_forwards(void){
-  
-    char msg = to_MotorMessage(0,1,1,0,0,0,1,1);
-    pWrite(msg);
-}
-
-//************************************************************
-//Go Backwards
-//
-//0100 0011 (forward, decreases front motor and increases rear motor)
-//************************************************************
-void go_backwards(void)
-{
-
-	char msg = to_MotorMessage(0,1,0,0,0,0,1,1);
-	pWrite(msg);
-}
-
-
-//************************************************************
-//Receives if the message is increasing(boolean), is in panic mode(boolean) 
-//and the char representing the binary of the 4 affected motors 
-//
-// returns the 8 bits as a char , representing the message
-//
-//Example:
-//<<46>> or 00100110
-//<<protocol:2 = 0, increase/decrease:1 = 1, mode:1 = 0, motors:4 = 6>>
-//************************************************************
+/*
+ *Receives if the message is increasing(boolean), is in panic mode(boolean) 
+ *and the char representing the binary of the 4 affected motors 
+ *
+ *returns the 8 bits as a char , representing the message
+ *Example:
+ *<<46>> or 00100110
+ *<<protocol:2 = 0, increase/decrease:1 = 1, mode:1 = 0, motors:4 = 6>>
+ */
 char to_MotorMessage(char ID0, char ID1, char increasing, char panicMode,
 					 char motor1,char motor2, char motor3, char motor4)
 {
-  char motors = 0;
+	char motors = 0;
 
-   if(ID0 == 1)
-    SET_FLAG(motors, BIT_POS(7)); 
+	if(ID0 == 1)
+		SET_FLAG(motors, BIT_POS(7)); 
   
-  if(ID1 == 1)
-    SET_FLAG(motors, BIT_POS(6));
+	if(ID1 == 1)
+		SET_FLAG(motors, BIT_POS(6));
 
-  if(increasing == 1)
-    SET_FLAG(motors, BIT_POS(5));
+	if(increasing == 1)
+		SET_FLAG(motors, BIT_POS(5));
 
-  if(panicMode == 1)
-    SET_FLAG(motors, BIT_POS(4));
+	if(panicMode == 1)
+		SET_FLAG(motors, BIT_POS(4));
 
-  if(motor1 == 1) 
-    SET_FLAG(motors, BIT_POS(3));
+	if(motor1 == 1) 
+		SET_FLAG(motors, BIT_POS(3));
 
-  if(motor2 == 1)
-    SET_FLAG(motors, BIT_POS(2));
+	if(motor2 == 1)
+		SET_FLAG(motors, BIT_POS(2));
 
-  if(motor3 == 1) 
-    SET_FLAG(motors, BIT_POS(1)) ;
+	if(motor3 == 1) 
+		SET_FLAG(motors, BIT_POS(1)) ;
 
-  if(motor4 == 1)
-    SET_FLAG(motors, BIT_POS(0)) ;
+	if(motor4 == 1)
+		SET_FLAG(motors, BIT_POS(0)) ;
   
-  return motors;
+	return motors;
 }
 
 //*******TEST METHODS
-void pWrite(char msg){
-
-  printf("\nProtocol has this written to it: ");
-  print_char_to_Binary(msg);
+void pWrite(char msg)
+{
+	printf("\nProtocol has this written to it: ");
+	print_char_to_Binary(msg);
 }
 
-void print_char_to_Binary(char bin){
+void print_char_to_Binary(char bin)
+{
+	char counter,temp,bit;
 
-  char counter,temp,bit;
+	counter =sizeof(bin) * 8;
 
-  counter =sizeof(bin) * 8;
+	for(counter = counter - 1; counter >= 0; counter--)
+		{
+			temp = 1 << counter;
+			bit = temp & bin;
+			if( bit == 0)
+				printf("0");
+			else
+				printf("1");
+		}
 
-  for(counter = counter - 1; counter >= 0; counter--)
-    {
-      temp = 1 << counter;
-      bit = temp & bin;
-      if( bit == 0)
-	printf("0");
-      else
-	printf("1");
-    }
-
-  printf("\n");
+	printf("\n");
 }
 
 //************End of Testing methods
