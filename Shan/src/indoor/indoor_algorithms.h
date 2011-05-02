df6c2c786ebf302a7348b77fc72cc0f2e7782905
e0 <zero42e@gmail.com>
2011-05-02 02:45:43 +0200

    Indoor using linked list. Key changes are data structure progressive_node and the function insert_progressive_node.

---
+++ Shan/src/indoor/indoor_algorithms.h
@@ -0,0 +1,24 @@
+/*
+ * indoor_algorithms.h
+ *
+ *  Created on: Apr 10, 2011
+ *      Author: Jon Fridehn
+ */
+
+#ifndef INDOOR_ALGORITHMS_H
+#define INDOOR_ALGORITHMS_H
+#include "path_structure.h"
+
+void AddNodeToOpen(node * current, nodeList * open);
+void RemoveNodeFromOpen(node * current, nodeList * open);
+void dijkstra_AddNeighborsToOpen(node * current, nodeList * open, nodeList * closed);
+void astar_AddNeighborsToOpen(node * current, nodeList * open, nodeList * closed, const position * end);
+void FreeAllocatedList(nodeList * list);
+int ListMemoryAllocation(nodeList ** open, nodeList ** closed);
+position_list CreateFinalList(nodeList * closed, const position * goal, const position * drone);
+int NodeInClosed(const int * nodeY, const int * nodeX, nodeList * closed);
+int NodeInOpen(const int * nodeY, const int * nodeX, nodeList * open);
+node * AddNodeToClosed(node * current, nodeList * closed);
+int GetHeuristicCost(const position * currentNode, const position * goalNode);
+
+#endif	// INDOOR_ALGORITHMS_H
---
+++ Shan/src/indoor/path_structure.h
@@ -0,0 +1,66 @@
+/*
+ * path_structure.h
+ *
+ * Authors: Jon Fridéhn Eric Britsman Shan Huang
+ */
+#ifndef PATH_STRUCTURE_H
+#define PATH_STRUCTURE_H
+
+#include <sys/time.h>
+#include <math.h>
+
+#define MAP_Y 14
+#define MAP_X 12
+#define NUM_ADJACENT 8
+#define CENTIMETRES_PER_TILE 108
+#define TILE_CENTER (CENTIMETRES_PER_TILE/2)
+#define NUM_ADJACENT 8
+
+typedef struct{
+    double lat;
+    double lon;
+    double angle;
+    double distance;
+} point;
+
+typedef struct progressive_node {
+    point p;
+	struct progressive_node *prev;
+	struct progressive_node *next;
+} progressive_node;
+
+
+typedef struct{
+	int y;
+	int x;
+}position;
+
+typedef struct node
+{
+	position pos;
+	int totalCost, heuristic;
+	position previous;
+}node;
+
+typedef struct {
+	node * list;
+	int count;
+}nodeList;
+
+typedef struct {
+	point * list;
+	int num;
+}position_list;
+
+position_list indoor_dijkstra(const position * start, const position * end);
+position_list indoor_astar(const position * start, const position * end);
+
+void set_direction();
+void set_distance();
+void update_position();
+void init_path(position start, position end);
+void navigate_path();
+void free_progressive_list();
+void insert_progressive_node();
+
+#endif /* PATH_STRUCTURE_H */
