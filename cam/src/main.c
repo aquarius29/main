4c03fb8c088ad5a3382bf30c45ae21f4b6cbd2a5
henry <henry@henry2.(none)>
2011-05-05 19:00:07 +0200

    Added integrated modules for video streaming and  modified the README file.

---
+++ cam/src/main.c
@@ -0,0 +1,14 @@
+/*-------------------------------------------------------------------------------------------------------
+**	main.c
+**	Authors: Henry Ikuh, Abdirashid Jama
+*-------------------------------------------------------------------------------------------------------*/
+
+int main(int argc, char** argv){
+
+void *video_server(void* arg);
+void terminate(char* msg, int retval);
+
+capture();
+//void capt()
+
+}
