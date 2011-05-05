e6925a686a80d1f772d66df3f32bea2f0f9573c3
jama <abraashg@gmail.com>
2011-05-05 13:38:17 +0200

    trying to restructure part 2

--- v4l/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PKG_CONFIG_PATH=/opt/ffmpeg/lib/pkgconfig
-rm *.o video-test &> /dev/null
-gcc `pkg-config --cflags libavformat libavcodec libavutil` -c v4l-test.c -o v4l-test.o
-gcc `pkg-config --cflags libavformat libavcodec libavutil` -c encode.c -o encode.o
-gcc `pkg-config --libs libavformat libavcodec libavutil` -o v4l-test v4l-test.o encode.o
--- v4l/encode.h
+++
@@ -1,3 +0,0 @@
-#include "v4l-test.h"
-
-void encode(struct buffer *b);
