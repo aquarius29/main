a40e7546e19f09a4d23bf6bfc2c4d63a5d567c26
My Högblom <my.hogblom@gmail.com>
2011-05-09 14:40:56 -0400

    added msg sending to stab_main

--- stab/src/stab_main.c
+++ stab/src/stab_main.c
@@ -13,6 +13,8 @@
  *    26/04/2011: Magn and accel is now read from here / integrated //Adam
  *    27/04/2011: Barometer code included //Adam
  *    08/05/2011: Removed some stuff and while loop //Adam
+ *    09/05/2011: Completed message passing to protocol, 
+ *                fixed some coding standards //My
  **************************************************************************/
 #ifdef PC 
 #include <stdio.h>
@@ -21,6 +23,7 @@
 #include "stab_interface.h"
 #include "stab.h"
 #include "math.h"
+#include <proto_lib.h>
 #elif defined ARDUINO 
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,6 +33,7 @@
 #include <Wire.h>
 #include <WProgram.h>
 #include "math.h"
+#include <proto_lib.h>
 #endif 
 /************************************************************
  * Global variables used to save input/output of the algorithm:
@@ -39,16 +43,16 @@
  ************************************************************/
 struct vector
 {
-  float x;
-  float y;
-  float z;
+    float x;
+    float y;
+    float z;
 };
 
 struct baro_data
 {
-  int temp;
-  long pressure;
-  float height;
+    int temp;
+    long pressure;
+    float height;
 };
 
 struct vector gyro_vect;
@@ -59,6 +63,8 @@ struct vector filter_vect;
 float heading; // heading from magnetometer
 void convert_accel_raw_to_deg();
 float data(float x, float y, float z);
+
+
 /*
  * Inits the main hardware components of the shield
  * A LOT OF CHANGES EXPECTED 
@@ -67,17 +73,17 @@ int16_t stab_init(void)
 {
   // code to init the hardware goes in here as well
 #if defined PC 
-  init_filter();
+    init_filter();
 #elif defined ARDUINO
-  init();
-  init_filter();
-  Serial.begin(9600);
-  Wire.begin();
-  init_gyro_hardware();
-  init_magn_hardware();
-  init_baro_hardware();
+    init();
+    init_filter();
+    Serial.begin(9600);
+    Wire.begin();
+    init_gyro_hardware();
+    init_magn_hardware();
+    init_baro_hardware();
 #endif
-  return 0; 
+    return 0; 
 
 }
 
@@ -90,71 +96,70 @@ int16_t stab_init(void)
 int16_t stab_run(void)
 {
 #ifdef PC
-  gyro_vect = init_sim();
-  accel_vect = init_sim();
+    gyro_vect = init_sim();
+    accel_vect = init_sim();
   
-  filter_vect.x = comp_filter(accel_vect.x, gyro_vect.y, filter_vect.x); // filtered pitch angle
-  filter_vect.y = comp_filter(accel_vect.y, gyro_vect.x, filter_vect.y); // filtered roll angle
-  filter_est[2] = comp_filter(acc_vector[2], gyro_vect[2], filter_est[2]); // filtered yaw angle
-  filter_vect.z = gyro_vect.z;
+    filter_vect.x = comp_filter(accel_vect.x, gyro_vect.y, filter_vect.x); // filtered pitch angle
+    filter_vect.y = comp_filter(accel_vect.y, gyro_vect.x, filter_vect.y); // filtered roll angle
+    filter_est[2] = comp_filter(acc_vector[2], gyro_vect[2], filter_est[2]); // filtered yaw angle
+    filter_vect.z = gyro_vect.z;
   
-  printf("ESTIMATED X %f\n", filter_vect.x);
-  printf("ESTMIATED Y %f\n", filter_vect.y);
-  printf("ESTMIATED Z %f\n", filter_vect.z);
+    printf("ESTIMATED X %f\n", filter_vect.x);
+    printf("ESTMIATED Y %f\n", filter_vect.y);
+    printf("ESTMIATED Z %f\n", filter_vect.z);
   
 #elif defined ARDUINO
-      gyro_vect =  read_gyro_data();
-      accel_vect = readAccel();
-      convert_accel_raw_to_deg();
-      magn_vect = read_magn_data();
-      heading = (atan2(magn_vect.y , magn_vect.x)+M_PI)*180/M_PI;
-      baro = read_baro_data();
+    gyro_vect =  read_gyro_data();
+    accel_vect = readAccel();
+    convert_accel_raw_to_deg();
+    magn_vect = read_magn_data();
+    heading = (atan2(magn_vect.y , magn_vect.x)+M_PI)*180/M_PI;
+    baro = read_baro_data();
       
-      Serial.println("Gyro data now:");
-      Serial.println(gyro_vect.x);
-      Serial.println(gyro_vect.y);
-      Serial.println(gyro_vect.z);
-      Serial.println();
+    Serial.println("Gyro data now:");
+    Serial.println(gyro_vect.x);
+    Serial.println(gyro_vect.y);
+    Serial.println(gyro_vect.z);
+    Serial.println();
       
-      Serial.println("Accel data now:");
-      Serial.println(accel_vect.x);
-      Serial.println(accel_vect.y);
-      Serial.println(accel_vect.z);
-      Serial.println();
-      
-      float d = data(magn_vect.x, magn_vect.y, magn_vect.z);
-      Serial.println("Magnetometer data now:");
-      Serial.println(magn_vect.x);
-      Serial.println(magn_vect.y);
-      Serial.println(magn_vect.z);
-      Serial.println(heading);
-      Serial.print("The constant is: ");
-      Serial.println(d);
-      Serial.println();
+    Serial.println("Accel data now:");
+    Serial.println(accel_vect.x);
+    Serial.println(accel_vect.y);
+    Serial.println(accel_vect.z);
+    Serial.println();
+    
+    float d = data(magn_vect.x, magn_vect.y, magn_vect.z);
+    Serial.println("Magnetometer data now:");
+    Serial.println(magn_vect.x);
+    Serial.println(magn_vect.y);
+    Serial.println(magn_vect.z);
+    Serial.println(heading);
+    Serial.print("The constant is: ");
+    Serial.println(d);
+    Serial.println();
 
-      Serial.println("Barometer data now:");
-      Serial.println(baro.temp);
-      Serial.println(baro.pressure);
-      Serial.println();
-
-      filter_vect.x = comp_filter(accel_vect.x, gyro_vect.y, filter_vect.x); // filtered pitch angle
-      filter_vect.y = comp_filter(accel_vect.y, gyro_vect.x, filter_vect.y); // filtered roll angle
-      filter_vect.z = comp_filter(heading, gyro_vect.z, filter_vect.z); // filtered yaw angle
+    Serial.println("Barometer data now:");
+    Serial.println(baro.temp);
+    Serial.println(baro.pressure);
+    Serial.println();
+    filter_vect.x = comp_filter(accel_vect.x, gyro_vect.y, filter_vect.x); // filtered pitch angle
+    filter_vect.y = comp_filter(accel_vect.y, gyro_vect.x, filter_vect.y); // filtered roll angle
+    filter_vect.z = comp_filter(heading, gyro_vect.z, filter_vect.z); // filtered yaw angle
+     
+    proto_stabWriteAttitude(filter_vect.y, filter_vect.x, filter_vect.z);
+    proto_stabWriteAcc(accel_vect.x, accel_vect.y, accel_vect.z);
+    proto_stabWriteHeading((int)heading);
+    proto_stabWriteHeight(baro.height);
       
-      //proto_stabWriteAttitude(filter_vect.y, filter_vect.x, filter_vect.z);
-      //proto_stabWriteAcc(accel_vect.x, accel_vect.y, accel_vect.z);
-      //proto_stabWriteHeading((int)heading);
-      //proto_stabWriteHeight(baro.height);
-      
-      Serial.println("Filtered data now:");
-      Serial.println(filter_vect.x);
-      Serial.println(filter_vect.y);
-      Serial.println(filter_vect.z);
-      Serial.println();
+    Serial.println("Filtered data now:");
+    Serial.println(filter_vect.x);
+    Serial.println(filter_vect.y);
+    Serial.println(filter_vect.z);
+    Serial.println();
 
 #endif
   
-  return 0;
+    return 0;
 }
 
 
@@ -164,14 +169,13 @@ int16_t stab_run(void)
 ************************************************************/
 void convert_accel_raw_to_deg()
 {
-  float R = sqrt((pow(accel_vect.x, 2))+(pow(accel_vect.y, 2))+(pow(accel_vect.z, 2)));
-  accel_vect.x = acos(accel_vect.x/R);
-  accel_vect.y = acos(accel_vect.y/R);
-  accel_vect.z = acos(accel_vect.z/R);
-
+    float R = sqrt((pow(accel_vect.x, 2))+(pow(accel_vect.y, 2))+(pow(accel_vect.z, 2)));
+    accel_vect.x = acos(accel_vect.x/R);
+    accel_vect.y = acos(accel_vect.y/R);
+    accel_vect.z = acos(accel_vect.z/R);
 }
 
 float data(float x, float y, float z)
 {
-  return sqrt((pow(x, 2))+(pow(y, 2))+(pow(z, 2)));
+    return sqrt((pow(x, 2))+(pow(y, 2))+(pow(z, 2)));
 }
