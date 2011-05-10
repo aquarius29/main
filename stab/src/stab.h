8b3858d5ecd9d95cc7f4762c6ef653daccf8d590
Yanling Jin <jjisjj@gmail.com>
2011-05-10 16:04:01 +0200

    ok

--- stab/src/stab.h
+++ stab/src/stab.h
@@ -17,17 +17,10 @@ float read_gyro();
 float read_accel();
 struct vector init_sim(void);
 #elif ARDUINO
-float comp_filter(float acc_scal, float gyro_scal, float old_est);
-void init_filter();
 void init_gyro_hardware();
-void init_magn_hardware();
 struct vector read_gyro_data();
-struct vector readAccel();
-struct vector read_magn_data();
+void readAccel();
 void read_stream(byte addr, int bytes_to_read, byte buffer[]);
-void write_data(byte addr, byte value, byte sensor_addr);
-void init_baro_hardware();
-struct baro_data read_baro_data();
 #endif	
 
 
--- stab/src/stab_ra.c
+++ stab/src/stab_ra.c
@@ -12,13 +12,6 @@
 #include <Wire.h>
 #include "WProgram.h"
 
-struct vector
-{
-  float x;
-  float y;
-  float z;
-};
-
 /* 
  *address of the accelerometer on the board 
  */
@@ -28,41 +21,66 @@ struct vector
  *Function prototypes
  */
 void init_accel();
-struct vector readAccel();
+void readAccel();
+
+
+
+/*int main()*/
+/*//void setup()*/
+/*{*/
+/*  init();*/
+/*  Wire.begin();*/
+/*  Serial.begin(9600);*/
+/*// initBMA180() should be done once, it will write appropriate adjustments for range and mode. */
+/*//  init_accel();*/
+/*//  delay(2000);*/
+/*	while(1){*/
+/*  readAccel();*/
+/*  delay(300);*/
+/*	}*/
+/*  return 0;*/
+/*}*/
+
 
 /*Read data from accelerometer memory*/
-struct vector readAccel()
+void readAccel()
 {
-  struct vector vect;
-  unsigned int result;
-  int x,y,z;
-  int temp;
-  
+    unsigned int result;
+    int x,y,z;
+    int temp;
+
   Wire.beginTransmission(ADDRESS);
-  Wire.send(0x02); /*start of x LSB for reading data*/
+  Wire.send(0x02);  							/*start of x LSB for reading data*/
   Wire.endTransmission();
   Wire.requestFrom((int)ADDRESS, 7);
   
   if(Wire.available()==7)
-    {
-      int lsb = Wire.receive()>>2;
-      int msb = Wire.receive();
-      vect.x =(msb<<6)+lsb; 
-      if (x&0x2000) x|=0xc000;
-      /* shift 2 unused bits */
-      lsb = Wire.receive()>>2;
-      msb = Wire.receive();
-      vect.y =(msb<<6)+lsb;
-      if (y&0x2000) y|=0xc000;
-      lsb = Wire.receive()>>2;
-      msb = Wire.receive();
-      vect.z =(msb<<6)+lsb;
-      if (z&0x2000) z|=0xc000;
-      temp = Wire.receive();
-      if (temp&0x80) temp|=0xff00;
-    }
-  
-  return vect;
+  {
+    int lsb = Wire.receive()>>2;
+    int msb = Wire.receive();
+    x=(msb<<6)+lsb; 
+    if (x&0x2000) x|=0xc000;
+/* shift 2 unused bits */
+    lsb = Wire.receive()>>2;
+    msb = Wire.receive();
+    y=(msb<<6)+lsb;
+    if (y&0x2000) y|=0xc000;
+    lsb = Wire.receive()>>2;
+    msb = Wire.receive();
+    z=(msb<<6)+lsb;
+    if (z&0x2000) z|=0xc000;
+    temp = Wire.receive();
+    if (temp&0x80) temp|=0xff00;
+  }
+/*  result = Wire.endTransmission();*/
+/*  Serial.print("raw data: ");*/
+/*  Serial.print("X=");*/
+/*  Serial.print(x);*/
+/*  Serial.print(" Y=");*/
+/*  Serial.print(y);*/
+/*  Serial.print(" Z=");*/
+/*  Serial.print(z);*/
+/*  Serial.println();*/
 }
 
 /*
@@ -106,6 +124,7 @@ void init_accel()
     delay(10);
   }
 }
+
 
 #endif
 
