1729ad57415c61f6211006c2a11b6b2e1fd83b7f
Joakim Gross <joakimgross@hotmail.com>
2011-05-12 09:50:24 +0200

    added more files in new folder structure

---
+++ sched/usart_isr/test_panda/src/port_test.c
@@ -0,0 +1,45 @@
+
+#include "proto_serial_port.h"
+#include "proto_serial_comm.h"
+#include "proto_serializer.h"
+#include "proto_msg_structs.h"
+#include "port_test.h"
+
+#include <stdio.h>
+
+static struct navData navMsg;
+    
+void port_test(void){
+    int32_t portHandle;
+
+    portHandle = proto_serialOpen();
+    
+    navMsgFill1();
+    printf("sending data1\n");
+    proto_serialSendNavMsg(portHandle, &navMsg);
+        
+    navMsgFill2();
+    printf("sending data2\n");
+    proto_serialSendNavMsg(portHandle, &navMsg);
+        
+    sleep(2);
+
+    proto_serialClose(portHandle);
+}
+
+void navMsgFill1(void){
+  navMsg.type = 10;
+  navMsg.order = 2;
+  navMsg.height = 1027;
+  navMsg.distance = 1027;
+  navMsg.yaw = 1027;
+}
+
+void navMsgFill2(void){
+  navMsg.type = 50;
+  navMsg.order = 2;
+  navMsg.height = 1027;
+  navMsg.distance = 1027;
+  navMsg.yaw = 1027;
+}
+
