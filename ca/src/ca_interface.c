efd8342928256b5a082926948c774aed1c3c935c
Jarryd Hall <jarryd.a.hall@gmail.com>
2011-05-12 11:10:40 +0200

    Updated repo

---
+++ ca/src/ca_interface.c
@@ -0,0 +1,86 @@
+/*****************************************************************************
+ * Product: movementInterface.c
+ * Version: 0.1
+ * Created: April 7, 2011
+ * History:
+ *          
+ *
+ * Movement/CA Group
+ *****************************************************************************/
+#include "ca_interface.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef ARDUINO
+#include <WProgram.h>
+
+//************************************************************
+// ARDUINO
+// All collision preperation goes here.
+//************************************************************
+int ca_init(void)
+{
+  return 1;
+}
+
+
+//************************************************************
+// ARDUINO
+// Collision is started here
+//************************************************************
+int ca_run(void)
+{
+  init();
+
+  /* //init the serial output */
+  // serial_init();
+  // fdevopen(serial_putchar, serial_getchar);
+
+  int i=0;
+  while(i<1000)
+    {
+      direction_filter();
+      i++;
+    }
+
+  return 1;
+
+}
+
+
+#elif defined PC
+//************************************************************
+// PC
+//  All movement preperation goes here.
+//************************************************************
+int ca_init(void)
+{
+  
+
+  return 1;
+
+
+}
+
+//************************************************************
+// PC
+// Movement is started here
+//************************************************************
+int ca_run(void)
+{
+
+  int ir1=100;
+  int ir2=100;
+  int ir3=100;
+  int ir4=100;
+  int i=0;
+  while (i<5){
+    direction_filter(get_speed(),get_dir(),ir1, ir2, ir3, ir4);
+    i++;
+  }
+
+  return 1;
+}
+
+
+#endif
---
+++ sched/src/sched_interface.h
@@ -0,0 +1,15 @@
+
+/*!
+ *  @file sched_interface.h
+ *  @brief Schedulers interface
+ *  @author Joakim
+ *  @date 2011-04-14
+ */
+
+
+#ifndef SCHED_INTERFACE_H
+#define SCHED_INTERFACE_H
+
+int sched_naive_main(void);
+
+#endif /* SCHED_INTERFACE_H */
