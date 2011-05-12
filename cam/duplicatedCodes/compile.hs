d5901689aec1a366a3318996c466f9a2f4751f5f
Maryam Aghaee <gusaghaema@student.gu.se>
2011-05-12 13:51:53 +0200

    coding standard

--- cam/duplicatedCodes/compile.hs
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PKG_CONFIG_PATH=/opt/ffmpeg/lib/pkgconfig
-rm *.o test &> /dev/null
-gcc `pkg-config --cflags libavformat` -c camStatus.c -o camStatus.o
-gcc `pkg-config --libs libavformat` camStatus.o -o camStats
