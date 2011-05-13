70c5080d6596a1b24b47f2b8b2236c024f4a0ffc
Taher <odeh@student.gu.se>
2011-05-13 10:21:57 +0200

    makefile added again, grr

--- psched/src/sched_proc_test.c
+++ psched/src/sched_proc_test.c
@@ -5,20 +5,20 @@
 #define SLEEPT 500*1000
 
 
-int16_t init();
-int16_t run();
+int16_t init1();
+int16_t run1();
 
 //int main(void){
 //  init();
 //  run();
 //}
 
-int16_t init(void){
+int16_t init1(void){
   printf("initialized...\n");
   return 1;
 }
 
-int16_t run(void){
+int16_t run1(void){
   int i = 4;
   while(i > 0){
     printf("ready to rumble...\n");
