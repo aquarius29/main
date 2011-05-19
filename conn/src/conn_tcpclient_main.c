a35b9271f711553895f9f993efe1f89aaec81684
Eugene Groshev <izimbra@student.gu.se>
2011-05-19 16:36:16 +0200

    Michal's conn code update after code review

--- conn/src/conn_tcpclient_main.c
+++ conn/src/conn_tcpclient_main.c
@@ -3,21 +3,18 @@
  *
  *  @brief Main function for client socket
  *
- *  @author Michal Musialik
+ *  @author Michal Musialik,Ali Issa
  *  
  *  @date 2011-04-10
- *              2011-05-07 - uppdating  conn_tcpclient_main.c to new standard
- *  @histroy  2011-05-11 - adding signal handler - Ali Issa
+ *  @history  2011-05-07 - uppdating conn_tcpclient_main.c - Michal,Ali
 */
 
 #include "conn_tcpclient_main.h"
 #include "conn_tcpclient.h"
 
  
- void init(void){
-   socket_create();
-  socket_binding();
-  socket_connection();
- 
- 
+void init(void){
+     socket_create();
+     socket_binding();
+     socket_connection();
 }
--- conn/src/main.c
+++ conn/src/main.c
@@ -1,4 +1,12 @@
-
+/*!
+ *  @file main.c
+ *
+ *  @brief main for server
+ *
+ *  @author Michal Musialik
+ *  @date 2011-05-07
+ * 
+ */
 #include "conn_tcpserver_main.h"
 
 int main(void){
